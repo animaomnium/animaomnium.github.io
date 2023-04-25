103c33e6d6036bc50f2bff4411186de340feb0e0
Anima Omnium <119851531+animaomnium@users.noreply.github.com>
2023-04-25 09:06:58 -0300

    Update graph.js

--- static/graph.js
+++ static/graph.js
@@ -3,11 +3,72 @@ const SPACE = 20;
 const canvas = document.getElementById("graph");
 const ctx = canvas.getContext("2d");
 
-const rotate = (x, y, rad) => {
-  const nx = Math.cos(rad * x) - Math.sin(rad * y);
-  const ny = Math.sin(rad * x) - Math.cos(rad * y);
-  return (nx, ny);
+const ROPE = 0;
+const PRT1 = 1;
+const PRT2 = 2;
+const NCON = 3;
+const NDUP = 4;
+const FREE = 5;
+
+// kind, x, y, connections
+let graph = [
+  [ROPE, 100, 100, [2, 1]],
+  [ROPE, 100, 200, [0, 2]],
+  [ROPE, 200, 100, [1, 0]], 
+];
+
+let reuse = [];
+let inser = [];
+let fresh = [];
+
+const neighbors = cons => {
+  cons.map(index => graph[index]);
 }
+
+const is_node = kind => {
+  return (kind == NCON || kind == NDUP); 
+}
+
+const alloc = node => {
+  if reuse.length === 0 {
+    fresh.push(node);
+    return graph.length + fresh.length;
+  } else {
+    const index = reuse.pop();
+    inser.push([index, node]);
+    return index;
+  }
+}
+
+const update = (i, n) => {
+  [k, x, y, c] = n;
+  if (k === FREE) { return node; }
+  const cons = neighbors(c);
+
+  if (k === ROPE) { 
+    let [[k1, x1, y1, _], 
+         [k2, x2, y2, _]] = cons;
+    if (is_node(k1) || is_node(k2)) {
+      reuse.push(
+      // TODO
+    }
+  }
+
+  if (k === PRT1) { }
+  if (k === PRT2) { }
+  if (k === NCON) { }
+  if (k === NDUP) { }
+}
+
+const update_all = now => {
+  let new_graph = [];
+  let i = 0;
+  for (const [index, node] in graph.entries()) {
+    new_node = update(index, node);
+    new_graph.push(new_node);
+  }
+  graph = new_graph;
+};
 
 // Draws a triangle with an active port at (x, y).
 // when rot == 0, triangle points down.
@@ -27,8 +88,9 @@ const triangle = (is_white, x, y, turn) => {
 
 const draw = now => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  const rot = now / 1000.0 % 1.0;
-  triangle(true, 100, 100, rot);
+  update_all(now);
+
+  // triangle(true, 100, 100, rot);
   window.requestAnimationFrame(draw);
 }
 
